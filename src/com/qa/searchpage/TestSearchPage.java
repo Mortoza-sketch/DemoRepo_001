dc5525982077231774e691b552e930da80804590
Mortoza-sketch <59904679+Mortoza-sketch@users.noreply.github.com>
2020-07-01 02:17:45 -0400

    Update TestSearchPage.java

--- src/com/qa/searchpage/TestSearchPage.java
+++ src/com/qa/searchpage/TestSearchPage.java
@@ -3,6 +3,8 @@ public class TestSearchPage {
 public static void main(String[] args) {
 
     System.out.println("This is a Test for Search Page");
+    System.out.println("I am going to make some changes here in Git Repository");
+    System.out.println("By using pull command the chnage should be updated accordingly in Eclipse IDE"); 
 	 
 	}
 
