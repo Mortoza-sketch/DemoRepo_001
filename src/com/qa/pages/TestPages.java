98e1d1183eec43bcbe780995a4e1e628355ab25f
Mortoza-sketch <59904679+Mortoza-sketch@users.noreply.github.com>
2020-07-01 02:09:22 -0400

    Update TestPages.java

--- src/com/qa/pages/TestPages.java
+++ src/com/qa/pages/TestPages.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
 	
 	System.out.println("This is a Test to check all the pages");
         System.out.println("I am going to make some changes here in Git Repository");
-        System.out.println("By using pull command the chnage should be update accordingly in Eclipse IDE"); 
+        System.out.println("By using pull command the chnage should be updated accordingly in Eclipse IDE"); 
 
 	}
 
